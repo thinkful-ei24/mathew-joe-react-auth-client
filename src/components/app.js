366ca59e40487563d55ae45c567fa0981e6ce05c
Joe-Pena <joepena@protonmail.com>
2018-10-11 15:53:45 -0600

    pushy

--- src/components/app.js
+++ src/components/app.js
@@ -7,16 +7,48 @@ import LandingPage from './landing-page';
 import Dashboard from './dashboard';
 import RegistrationPage from './registration-page';
 import {refreshAuthToken} from '../actions/auth';
+import {clearAuth} from '../actions/auth';
+import {clearAuthToken} from '../local-storage';
 
 export class App extends React.Component {
     componentDidUpdate(prevProps) {
         if (!prevProps.loggedIn && this.props.loggedIn) {
             // When we are logged in, refresh the auth token periodically
             this.startPeriodicRefresh();
+            //setTimeout to log out
+            this.logOutTimeoutRefresh();
         } else if (prevProps.loggedIn && !this.props.loggedIn) {
             // Stop refreshing when we log out
             this.stopPeriodicRefresh();
         }
+    }
+
+    logOutTimeoutRefresh() {
+
+        function promiseTimeout (time) {
+            return new Promise(function(resolve,reject){
+              setTimeout(() => {
+                  resolve(time);
+                },time);
+            });
+          };
+        
+        function confirm() {
+            const warning = window.confirm('You will be logged out in 1 minute, press okay to stay logged in, press cancel to delete system32');
+            return warning;
+        }
+
+        promiseTimeout(3000)
+        .then(() => {if (confirm()) {
+            this.logOutTimeoutRefresh();
+            } else {
+                this.timeoutInterval = setTimeout(
+                () => {this.props.dispatch(clearAuth())
+                clearAuthToken()},
+                2000 //5 secs
+                )
+            }
+        })
     }
 
     componentWillUnmount() {
@@ -26,8 +58,9 @@ export class App extends React.Component {
     startPeriodicRefresh() {
         this.refreshInterval = setInterval(
             () => this.props.dispatch(refreshAuthToken()),
-            60 * 60 * 1000 // One hour
+            60 * 10 * 1000 // One hour
         );
+        console.log(this.refreshInterval);
     }
 
     stopPeriodicRefresh() {
